05d037f553b4824dd3ea74ee28dde0ac002f413f
hoangcuong <cuongbkvn@gmail.com>
2012-09-11 15:48:12 +0700

    Intinial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidu::Application.config.secret_token = '97df508ee25707efb86c2458eb1500c518176efb4d1c9c63e18f0de54cb058fdd319510916dff9c4a5fb0e9078694e05f2edab52a7af99da6ba938239adb1b09'
